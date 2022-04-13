0f1e0354e7e6ef84819198b325b97bb8323d147a
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-04-13 11:45:27 +0200

    Create Tahak_C#.cs

---
+++ Tahak_C#.cs
@@ -0,0 +1,24 @@
+Console.WriteLine("neco); //Printování něčeho
+Convet.ToInt32(Console.Readline()); //Input něčeho
+Var neco = 55.5; //variabl nemusis pouzit double, pozna to samo
+const float = 2.5f; //float je staly, nelze jiz zmenit
+if(neco > neco){
+}
+switch (neco){
+case 38:
+console.writeLine("ano");
+break;
+default; //neco jako else u if}
+while loop; //dokud to plati
+for (urceni; loop; co se deje)
+for(;;){} // nekonecny loop
+//Methody se muzou volat jinou metodou
+static void metoda(int x){ // urci se metoda s x
+}
+static void halo(){
+metoda(40) //volam metodu "metoda" a urcim x jako 40}
+//void metoda nevyzaduje return, zatimco ostatni musi, třeba int
+//Metoda může volat sama sebe a nazýva se to exit condition
+//class se vytvaří - "class jmeno{obsah}"
+//instance new vytvaří objekt
+/* Encapsulation - ma tyto acces modifikace public, private, protected, internal, protected internal
